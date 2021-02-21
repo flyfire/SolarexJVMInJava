59948a03cbc0130b986ea194792e77a20e191911
Solarex <rh.hou.work@gmail.com>
2021-02-21 15:49:54 +0800

    ch02

---
+++ src/main/java/ch01/Sum10.java
@@ -0,0 +1,15 @@
+package ch01;
+
+/**
+ * Created by Solarex at 1:11 PM/2/20/21
+ * Desc:
+ */
+public class Sum10 {
+    public static int sum10() {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
